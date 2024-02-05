c6f5b20a98d085a7bba10b15a13c63493c7fc4d9
Tantatorn <timefear007@gmail.com>
2024-02-05 22:19:27 +0700

    ✨ add reverse linked list 2

--- pkg/medium/reverse_linked_list.go
+++ pkg/medium/reverse_linked_list.go
@@ -6,28 +6,30 @@ func reverseBetween(head *common.ListNode,
 	left int, right int) *common.ListNode {
 	node := head
 
-	var leftLink *common.ListNode
-	var rightLink *common.ListNode
+	var nums []int
+	i := 0
 
-	l := 0
-	r := 0
-
+	// first scan
 	for node != nil {
-		l++
-		r++
-		if l+1 == left {
-			leftLink = node
-		}
-		if r+1 == right {
-			rightLink = node.Next
+		if i >= left-1 && i < right {
+			nums = append(nums, node.Val)
 		}
 
 		node = node.Next
+		i++
 	}
 
-	//temp := leftLink
-	//leftLink.Next = rightLink
-	//rightLink.Next = leftLink
+	// second scan
+	node = head
+	i = 0
+	for node != nil {
+		if i >= left-1 && i < right {
+			node.Val = nums[right-i-1]
+		}
+
+		node = node.Next
+		i++
+	}
 
 	return head
 }
